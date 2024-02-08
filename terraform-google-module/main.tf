f9801a3afc360cbbba07f4c6aff31a33f7adab6a
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-02-08 13:32:17 +0200

    Create main.tf

---
+++ terraform-google-module/main.tf
@@ -0,0 +1,42 @@
+
+variable "run_id" {
+  default = "run_id_12"
+}
+
+variable "sleep_time" {
+  default = 60
+}
+
+resource "random_integer" "timeout" {
+  min = 32
+  max = 180
+
+  keepers = {
+    run_id = var.run_id
+  }
+}
+
+
+
+resource "null_resource" "env_vars" {
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "env"
+  }
+}
+
+output "very_long" {
+  value = "Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
+}
+
+output "senc_out" {
+  value = "secret data"
+  description = "my sensitive output"
+  sensitive = true
+}
+
+output "output_run_id" {
+  value = var.run_id
+}
